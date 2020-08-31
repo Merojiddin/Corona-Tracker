e316ad6022c00e8232605fefc1b6ce4639ff120a
Merojiddin <merojiddin_1998@mail.ru>
2020-09-01 00:32:38 +0800

    Updates

---
+++ precache-manifest.2efce44c9ed13c90cf4f47cce63c5aea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5165e252d127276c0efb34744ea24bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53f0942e7f82445f69f2",
+    "url": "/static/css/main.d2a589b9.chunk.css"
+  },
+  {
+    "revision": "31738e41e20a8a59d66c",
+    "url": "/static/js/2.ce3d45ac.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/static/js/2.ce3d45ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53f0942e7f82445f69f2",
+    "url": "/static/js/main.a00f5498.chunk.js"
+  },
+  {
+    "revision": "b7550932cdc1310782ca",
+    "url": "/static/js/runtime-main.1142ef6f.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
